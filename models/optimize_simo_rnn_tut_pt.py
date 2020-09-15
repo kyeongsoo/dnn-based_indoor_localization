83a8fafaebee5453d977a474df86cdde3aab9ca6
Kyeong Soo (Joseph) Kim <kyeongsoo.kim@gmail.com>
2020-09-15 15:56:09 +0800

    Refine the script.

--- models/optimize_simo_rnn_tut_pt.py
+++ models/optimize_simo_rnn_tut_pt.py
@@ -6,7 +6,6 @@
 
 import torch
 import numpy as np
-
 from ax.plot.contour import plot_contour
 from ax.plot.trace import optimization_trace_single_method
 from ax.service.managed_loop import optimize
@@ -14,20 +13,12 @@ from ax.utils.notebook.plotting import render, init_notebook_plotting
 #from ax.utils.tutorials.cnn_utils import load_mnist, train, evaluate, CNN
 from simo_rnn_tut_pt import simo_rnn_tut_pt
 
-init_notebook_plotting()
-
-
-# In[2]:
-
 
 torch.manual_seed(12345)
 dtype = torch.float
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 num_runs = 2
 mean_error_3ds = np.empty(num_runs)
-
-
-# In[3]:
 
 
 def train_evaluate(parameterization):
@@ -44,9 +35,11 @@ def train_evaluate(parameterization):
                               sdae_hidden_layers='',
                               cache=True,
                               rnn_hidden_size=parameterization.get('rnn_hidden_size'),
-                              rnn_num_layers=1,
-                              floor_hidden_layers=[128],
-                              coordinates_hidden_layers=[128],
+                              rnn_num_layers=parameterization.get('rnn_num_layers'),
+                              floor_hidden_layers=[1024],
+                              # floor_hidden_layers=parameterization.get('floor_hidden_layers'),
+                              coordinates_hidden_layers=[1024],
+                              # coordinates_hidden_layers=parameterization.get('coordinates_hidden_layers'),
                               floor_weight=1.0,
                               coordinates_weight=1.0,
                               log_level='WARNING',
@@ -54,9 +47,6 @@ def train_evaluate(parameterization):
         mean_error_3ds[i] = rst.mean_error_3d
 
     return {"mean_3d_error": (mean_error_3ds.mean(), mean_error_3ds.std())}
-
-
-# In[ ]:
 
 
 best_parameters, values, experiment, model = optimize(
@@ -76,8 +66,27 @@ best_parameters, values, experiment, model = optimize(
             "name": "rnn_hidden_size",
             "type": "choice",
             "values": [16, 64, 128, 256, 512, 1024]
-        }
+        },
+        {
+            "name": "rnn_num_layers",
+            "type": "choice",
+            "values": [1, 2]
+        },
+        # {
+        #     "name": "floor_hidden_layers",
+        #     "type": "choice",
+        #     "values": [[256], [512], [1024]]
+        # },
+        # {
+        #     "name": "floor_hidden_layers",
+        #     "type": "choice",
+        #     "values": [[256], [512], [1024]]
+        # },
     ],
     evaluation_function=train_evaluate,
     objective_name='mean_3d_error',
 )
+
+
+print(best_parameters)
+print(values)
